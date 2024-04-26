07b73613629865ddb763df3bb5058fd4982c3935
safdar imam <safdarimam486@gmail.com>
2024-04-26 11:55:36 +0530

    commit

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ var arr = [1,2,3,4];
  arr.forEach(function(val){
 console.log(val + 2)
 })
-    
-
+    map
+let map1 = new Map([
+    [1 , 10], [2 , 20] ,
+    [3, 30],[4, 40]
+    ]);
+     
+console.log("Map1: ");
+console.log(map1);
 
 
